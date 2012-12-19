c761f3969f858669ddf2f72c317faf41eb67c975
Osbert Feng <ofeng@ozbert.com>
2012-12-18 16:55:20 -0800

    Change group ID for Clojars.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject hiccup-bootstrap "0.1.2"
+(defproject org.clojars.osbert/hiccup-bootstrap "0.1.2-SNAPSHOT"
   :description "Hiccup functions for Twitter's Bootstrap"
   :url "https://github.com/weavejester/hiccup-bootstrap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [hiccup "1.0.3"]
                  [compojure "1.1.5"]])
